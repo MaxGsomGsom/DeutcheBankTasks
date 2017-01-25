aef657a4b06c74c3d61e06b24379fdb33233e67d
Maxim Kuzmin <MaxGsomGsom@users.noreply.github.com>
2017-01-25 21:40:03 +0000

    init

---
+++ Task2_/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List list = new List("1");
+            list.link = new List("2");
+            list.link.link = new List("3");
+            list.link.link.link = new List("4");
+
+            var list2 = list;
+            while (list2 != null)
+            {
+                Console.WriteLine(list2.data);
+                list2 = list2.link;
+            }
+            Console.WriteLine("---");
+
+            List prev = null;
+            List cur = list;
+            List next = cur.link;
+            cur.link = null;
+
+            do
+            {
+                prev = cur;
+                cur = next;
+                next = next.link;
+                cur.link = prev;
+            } while (next != null);
+
+
+
+            list2 = cur;
+            while (list2 != null)
+            {
+                Console.WriteLine(list2.data);
+                list2 = list2.link;
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+
+    class List
+    {
+        public List(string data)
+        {
+            this.data = data;
+        }
+
+        public string data;
+        public List link;
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var visitors = new List<Visitor>();
+            var r = new Random();
+            int dayLength = 24 * 60;
+
+            for (int i = 0; i < 100; i++)
+            {
+                var newOne = new Visitor();
+                newOne.arrival = r.Next(0, dayLength - 10);
+                newOne.departure = r.Next(newOne.arrival, dayLength);
+                visitors.Add(newOne);
+            }
+
+            int[] helper = new int[visitors.Count()];
+
+            for (int i = 0; i < visitors.Count; i++)
+            {
+                for (int j = i+1; j < visitors.Count; j++)
+                {
+                    if (visitors[j].arrival < visitors[i].arrival && visitors[j].departure > visitors[i].departure)
+                        helper[i]++;
+                }
+            }
+
+            int max = 0;
+            for (int i = 0; i < visitors.Count; i++)
+            {
+                if (helper[i] > helper[max]) max = i;
+            }
+
+            Console.WriteLine("" + helper[max] + ": " + visitors[max].arrival + " - " + visitors[max].departure);
+            Console.ReadKey();
+        }
+
+
+    }
+
+    class Visitor
+    {
+        public int departure, arrival;
+    }
+}
